e0a3e86991da26fb9d43f9299ed75354f7138081
GregoryAlbarian <albarian@chapman.edu>
2019-12-22 15:58:16 -0800

    linted with Flake8

--- KaprekarKernel.py
+++ KaprekarKernel.py
@@ -1,5 +1,7 @@
-#this function return -1 if it possible_number is not a real number
-#this function works!
+# this function return -1 if it possible_number is not a real number
+# this function works!
+
+
 def input_validation(number):
     number = str(number)
     if len(number) != 4:
@@ -8,11 +10,14 @@ def input_validation(number):
         return False
     else:
         return True
-    
+
+
 # this function take the string digit_list and breaks it
 # into a list of 4 if there is not that many characters
-# in the string then it will add 0's 
-def break_into_list (digit_list):
+# in the string then it will add 0's
+
+
+def break_into_list(digit_list):
     digit_list = list(str(digit_list))
     length = len(digit_list)
     zeroes_needed = 4 - length
@@ -20,9 +25,14 @@ def break_into_list (digit_list):
     digit_list.extend(zeroes_list)
     return digit_list
 
+
+# assign place values to list digit_list to be able
+# to convert to an int
+
+
 def assign_place_values(digit_list):
     full_number = 0
-    for i in range(0,len(digit_list)):
+    for i in range(0, len(digit_list)):
         each_digit = int(digit_list[i])
         full_number += each_digit * 10 ** (len(digit_list) - i - 1)
 
@@ -41,8 +51,10 @@ def kaprekar_operation(digits):
     return (backwards_sort, sorted_list, backwards_sort-sorted_list)
 
 
-# main program starts here 
-number = input("Give a four digit, positive number in which not all the digits are alike: ")
+# main program starts here
+message = "Give a four digit, positive number "
+message += "in which not all the digits are alike: "
+number = input(message)
 
 while True:
     try:
@@ -50,14 +62,16 @@ while True:
             number = int(number)
             break
         else:
-            number=input("Try again: make sure the number is four digits, positive, and not all the numbers are the same: ")
+            message = "Try again: make sure the number is four digits, "
+            message += "positive, and not all the numbers are the same: "
+            number = input(message)
     except ValueError:
-        number = input ("Try again: give an integer: ")
+        number = input("Try again: give an integer: ")
 
 all_numbers = kaprekar_operation(number)
-print(all_numbers[0],"-",all_numbers[1],"=",all_numbers[2])
+print(all_numbers[0], "-", all_numbers[1], "=", all_numbers[2])
 
-while all_numbers[2]!=6174:
+while all_numbers[2] != 6174:
     all_numbers = kaprekar_operation(all_numbers[2])
-    print(all_numbers[0],"-",all_numbers[1],"=",all_numbers[2])
+    print(all_numbers[0], "-", all_numbers[1], "=", all_numbers[2])
 print("finished! You have reached 6174, the four-digit Kaprekar Kernel")
